6443511735f498cd5a04ec46dee5e905450ba381
Angel M de Miguel <angelmm@redborder.net>
2016-02-12 08:52:38 +0100

    #1 #2 Added a search box to the page. Improve user interface

--- app/assets/javascripts/pretty_routes/pretty_routes.js
+++ app/assets/javascripts/pretty_routes/pretty_routes.js
@@ -26,3 +26,62 @@ function setupRouteToggleHelperLinks() {
     setValOn(helperElems, helperTxt);
   });
 }
+
+$(document).ready(function(){
+  $('#search-routes').focus();
+});
+
+// Listener to search
+$(document).on('input', '#search-routes', function(){
+  var $el = $(this),
+      value = $el.val(),
+      found_count = 0;
+
+  if(value.length < 3) {
+    $('#route_table tbody tr').show();
+    $('tr.empty').hide();
+    $('span.highlight').each(function(){
+      $(this).replaceWith($(this).text());
+    });
+    return;
+  }
+
+  $('#route_table tbody tr.route_row').each(function(index) {
+    var $row = $(this),
+        found = false;
+
+    $row.find('td').each(function(){
+      var $col = $(this);
+
+      if (!$col.hasClass('verb')) {
+        var text = $col.html();
+        text = text.replace('<span class="highlight">', '');
+        text = text.replace('</span>', '');
+
+        if(text.indexOf(value) > -1){
+          text = text.replace(value, '<span class="highlight">' + value + '</span>');
+          found = true;
+        }
+        // Add text
+        $col.html(text);
+      }
+
+    });
+
+    if (found) {
+      $row.show();
+      found_count++;
+    } else {
+      $row.hide();
+    }
+
+  });
+
+  // Check if found_count
+  if (found_count == 0) {
+    $('tr.empty td').html('There are not any coincidence for <b>' + value + '</b>');
+    $('tr.empty').show();
+  } else {
+    $('tr.empty').hide();
+  }
+});
